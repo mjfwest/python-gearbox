687f3bcd782c4910e773759524186beab209fa40
mjfwest <martin@mjfwest.co.uk>
2015-05-26 13:17:17 +0200

    2to3 from fork

--- gearbox/doc/source/conf.py
+++ gearbox/doc/source/conf.py
@@ -51,9 +51,9 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'python-gearbox'
-copyright = u'2015, Eduardo M. Fírvida Donéstevez'
-author = u'Eduardo M. Fírvida Donéstevez'
+project = 'python-gearbox'
+copyright = '2015, Eduardo M. Fírvida Donéstevez'
+author = 'Eduardo M. Fírvida Donéstevez'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -227,8 +227,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'python-gearbox.tex', u'python-gearbox Documentation',
-     u'Eduardo M. Fírvida Donéstevez', 'manual'),
+    (master_doc, 'python-gearbox.tex', 'python-gearbox Documentation',
+     'Eduardo M. Fírvida Donéstevez', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -257,7 +257,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'python-gearbox', u'python-gearbox Documentation',
+    (master_doc, 'python-gearbox', 'python-gearbox Documentation',
      [author], 1)
 ]
 
@@ -271,7 +271,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'python-gearbox', u'python-gearbox Documentation',
+    (master_doc, 'python-gearbox', 'python-gearbox Documentation',
      author, 'python-gearbox', 'One line description of project.',
      'Miscellaneous'),
 ]
